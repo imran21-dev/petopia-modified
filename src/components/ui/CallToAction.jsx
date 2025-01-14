427e65c83d5fd786bd8e07fb153126800f9d01cb
Md Imran Parves <89092460+imran21-dev@users.noreply.github.com>
2025-01-14 10:16:38 +0600

    call to action section design complete

--- src/components/ui/CallToAction.jsx
+++ src/components/ui/CallToAction.jsx
@@ -1,10 +1,33 @@
 import Title from "./Title";
-
+import happyPet from '../../assets/happy pet.jpg'
+import { Link } from "react-router-dom";
+import { Button } from "./button";
+import SwiperImage from "./SwiperImage";
 
 const CallToAction = () => {
     return (
         <div>
             <Title title='Give Them a Second Chance' desc='Open your heart and home to a pet in need. Adoption not only transforms their lives but fills yours with unconditional love and companionship. Be the reason for their happily ever after!'></Title>
+
+            <div className="flex gap-10">
+                <figure className="w-2/4">
+                    <img src={happyPet} alt="" className="w-full rounded-3xl" />
+                </figure>
+                <div className="w-2/4">
+                    <h1 className="text-3xl opacity-70 font-bold">Every Pet Deserves a Loving Home</h1>
+                    <p className="py-3 opacity-70">By adopting a pet, you’re giving them a chance to experience love, care, and a better life. Join us in creating happy stories, one adoption at a time.</p>
+                    <ul className="list-inside list-disc opacity-70">
+                        <li>Save a life and provide a second chance.</li>
+                        <li>Gain a loyal and loving companion.</li>
+                        <li>Support ethical and humane practices.</li>
+                    </ul>
+
+                    <SwiperImage></SwiperImage>
+
+
+                    <Link><Button>View Available Pets</Button></Link>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/ui/SwiperImage.jsx
@@ -0,0 +1,108 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import pet1 from "../../assets/pet1.jpg";
+import pet2 from "../../assets/pet2.jpg";
+import pet3 from "../../assets/pet3.jpg";
+import pet4 from "../../assets/pet4.jpg";
+import pet5 from "../../assets/pet5.jpg";
+import pet6 from "../../assets/pet6.jpg";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination, Autoplay } from "swiper/modules";
+
+const SwiperImage = () => {
+  return (
+    <div className="py-10">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        loop='true'
+        autoplay={{
+          
+          delay: 2500,
+          disableOnInteraction: false
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="relative  swipperCard">
+            <img
+              src={pet1}
+              alt=""
+              className="w-full h-60 object-cover rounded-2xl "
+            />
+            <p className="absolute flex opacity-0 duration-300 swipperText bg-gradient-to-t from-background/50 to-transparent h-full  w-full items-end p-5 font-semibold backdrop-blur-sm bottom-0 rounded-2xl">
+              Adopt. Love. Transform Lives.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative  swipperCard">
+            <img
+              src={pet5}
+              alt=""
+              className="w-full h-60 object-cover rounded-2xl "
+            />
+            <p className="absolute flex opacity-0 duration-300 swipperText bg-gradient-to-t from-background/50 to-transparent h-full  w-full items-end p-5 font-semibold backdrop-blur-sm bottom-0 rounded-2xl">
+              Adopt. Love. Transform Lives.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative  swipperCard">
+            <img
+              src={pet6}
+              alt=""
+              className="w-full h-60 object-cover rounded-2xl "
+            />
+            <p className="absolute flex opacity-0 duration-300 swipperText bg-gradient-to-t from-background/50 to-transparent h-full  w-full items-end p-5 font-semibold backdrop-blur-sm bottom-0 rounded-2xl">
+              Adopt. Love. Transform Lives.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative  swipperCard">
+            <img
+              src={pet3}
+              alt=""
+              className="w-full h-60 object-cover rounded-2xl "
+            />
+            <p className="absolute flex opacity-0 duration-300 swipperText bg-gradient-to-t from-background/50 to-transparent h-full  w-full items-end p-5 font-semibold backdrop-blur-sm bottom-0 rounded-2xl">
+              Adopt. Love. Transform Lives.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative  swipperCard">
+            <img
+              src={pet4}
+              alt=""
+              className="w-full h-60 object-cover rounded-2xl "
+            />
+            <p className="absolute flex opacity-0 duration-300 swipperText bg-gradient-to-t from-background/50 to-transparent h-full  w-full items-end p-5 font-semibold backdrop-blur-sm bottom-0 rounded-2xl">
+              Adopt. Love. Transform Lives.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative  swipperCard">
+            <img
+              src={pet2}
+              alt=""
+              className="w-full h-60 object-cover rounded-2xl "
+            />
+            <p className="absolute flex opacity-0 duration-300 swipperText bg-gradient-to-t from-background/50 to-transparent h-full  w-full items-end p-5 font-semibold backdrop-blur-sm bottom-0 rounded-2xl">
+              Adopt. Love. Transform Lives.
+            </p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default SwiperImage;
